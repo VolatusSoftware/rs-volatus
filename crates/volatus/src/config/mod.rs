dbf28cf5cc0e51f3a4794a9fee7ab5a56a6f5d71
Derrick Bommarito <negentropicdev@gmail.com>
2026-08-16 21:10:24 -0700

    look ma, tests! Start implementing checks such as reserved names for elements.

--- crates/volatus/src/config/mod.rs
+++ crates/volatus/src/config/mod.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Value {
     None,
     Str(String),
@@ -13,11 +13,9 @@ pub enum Value {
     I64(i64),
     F32(f32),
     F64(f64),
-    Array, // Only directly store scalars, containers must use traversals
+    Array, // Only directly store scalars, containers use traversals
     Object,
 }
-
-type Hierarchy = Vec<String>;
 
 type ElementIdx = usize;
 
@@ -28,7 +26,13 @@ pub struct Manager {
 
 impl Manager {
     pub fn new() -> Manager {
-        Manager { elems: vec![] }
+        Manager {
+            elems: vec![
+                // Create root node as first element (idx == 0) to store names of top level elems
+                // It's created as an object since it will hold named elements.
+                Element::new("", Value::Object),
+            ],
+        }
     }
 
     pub fn create_element(
@@ -36,16 +40,27 @@ impl Manager {
         name: &str,
         value: Value,
         parent: Option<ElementIdx>,
-    ) -> ElementIdx {
+    ) -> Result<ElementIdx, String> {
+        if name.is_empty() || name == "Meta" || name == "Value" {
+            return Err(format!("Element name '{name}' cannot be empty or the reserved strings 'Meta' or 'Value'."));
+        }
+
         let idx = self.elems.len();
         self.elems.push(Element::new(name, value));
 
-        if let Some(p_idx) = parent {
-            self.elems[p_idx].children.push(idx);
-            self.elems[idx].parent = parent;
-        }
+        // New elements are always linked to from a parent
+        // If new element parent was not specified then use the root element
+        //  so that "top-level" names can be looked up.
+        let p_idx = parent.unwrap_or_else(|| 0);
+        self.elems[p_idx].children.push(idx);
 
-        idx
+        // New elements do not link back to the root node
+        // This simplifies hierarchy lookups and generation
+        if p_idx > 0 {
+            self.elems[idx].parent = Some(p_idx);
+        };
+
+        Ok(idx)
     }
 
     pub fn name(&self, idx: ElementIdx) -> &str {
@@ -56,7 +71,7 @@ impl Manager {
         &self.elems[idx].value
     }
 
-    pub fn lookup_element(&self, hierarchy: Hierarchy) -> Option<ElementIdx> {
+    pub fn lookup_element(&self, hierarchy: Vec<&str>) -> Option<ElementIdx> {
         let mut idx: ElementIdx = 0;
         let mut child_found;
         for name in hierarchy {
@@ -82,10 +97,11 @@ impl Manager {
         self.elems[idx].parent
     }
 
-    pub fn hierarchy_for(&self, e: ElementIdx) -> Hierarchy {
-        let mut h: Hierarchy = vec![];
+    pub fn hierarchy_for(&self, e: ElementIdx) -> Vec<String> {
+        let mut h = vec![];
         let mut idx = e;
 
+        // Follow parent index until no further parent links.
         loop {
             h.push(self.name(idx).to_owned());
             match self.elems[idx].parent {
@@ -94,7 +110,9 @@ impl Manager {
             }
         }
 
+        // Since we worked from child-most towards root, need to fix back from parent-most to element.
         h.reverse();
+
         h
     }
 }
@@ -109,6 +127,7 @@ struct Element {
 
 impl Element {
     fn new(name: &str, value: Value) -> Self {
+        // New elements start out orphaned and Manager will link appropriately.
         Element {
             name: name.to_owned(),
             value,
@@ -123,18 +142,51 @@ mod tests {
     use super::*;
 
     #[test]
+    fn empty_element_name_fails() {
+        let mut m = Manager::new();
+        assert!(m.create_element("", Value::None, None).is_err());
+    }
+
+    #[test]
+    fn reserved_element_name_fails() {
+        let mut m = Manager::new();
+        assert!(m.create_element("Meta", Value::None, None).is_err());
+        assert!(m.create_element("Value", Value::None, None).is_err());
+    }
+
+    #[test]
     fn create_element() {
         let mut m = Manager::new();
-        let e = m.create_element("alpha", Value::Str(String::from("Meowdy")), None);
+        let e = m.create_element("alpha", Value::Str(String::from("Meowdy")), None).unwrap();
         assert_eq!(m.name(e), "alpha");
     }
 
     #[test]
     fn create_child_get_hierarchy() {
         let mut m = Manager::new();
-        let parent = m.create_element("parent", Value::I32(42), None);
-        let child = m.create_element("child", Value::Bool(true), Some(parent));
+        let parent = m.create_element("parent", Value::I32(42), None).unwrap();
+        let child = m.create_element("child", Value::Bool(true), Some(parent)).unwrap();
 
         assert_eq!(m.hierarchy_for(child), vec!["parent", "child"]);
+    }
+
+    #[test]
+    fn traverse_to_parent() {
+        let mut m = Manager::new();
+        let p = m.create_element("parent", Value::None, None).unwrap();
+        let c = m.create_element("child", Value::None, Some(p)).unwrap();
+
+        let tp = m.parent_of(c).unwrap();
+        assert_eq!(m.name(tp), "parent");
+    }
+
+    #[test]
+    fn lookup_hierarchy() {
+        let mut m = Manager::new();
+        let p = m.create_element("parent", Value::None, None).unwrap();
+        let _c = m.create_element("child", Value::I32(42), Some(p)).unwrap();
+
+        let e = m.lookup_element(vec!["parent", "child"]).unwrap();
+        assert_eq!(*m.value(e), Value::I32(42));
     }
 }
--- crates/volatus/src/main.rs
+++ crates/volatus/src/main.rs
@@ -2,10 +2,16 @@ use volatus::config::{Manager, Value};
 
 fn main() {
     let mut m = Manager::new();
-    let p = m.create_element("alpha", Value::I32(42), None);
-    let c1 = m.create_element("beta", Value::Bool(true), Some(p));
-    let _l = m.create_element("gamma", Value::Str("Meowdy".to_owned()), Some(c1));
-    let _c2 = m.create_element("delta", Value::Bool(false), Some(p));
+    let p = m.create_element("alpha", Value::I32(42), None).unwrap();
+    let c1 = m
+        .create_element("beta", Value::Bool(true), Some(p))
+        .unwrap();
+    let _l = m
+        .create_element("gamma", Value::Str("Meowdy".to_owned()), Some(c1))
+        .unwrap();
+    let _c2 = m
+        .create_element("delta", Value::Bool(false), Some(p))
+        .unwrap();
 
-    println!("{m:?}");
+    println!("{m:#?}");
 }
