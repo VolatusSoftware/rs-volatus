17fe0ce05c707776f7a9cbda764b97be42ef59a7
Derrick Bommarito <negentropicdev@gmail.com>
2026-08-19 20:55:42 -0700

    JSON parsing!!!11!11!!!

--- crates/volatus/src/config/mod.rs
+++ crates/volatus/src/config/mod.rs
@@ -1,12 +1,11 @@
 use std::{
-    collections::{BTreeMap, VecDeque},
-    fmt::Debug,
+    collections::{BTreeMap, VecDeque}, fmt::Debug,
 };
 
-use serde_json;
+use serde_json::{Value, Map};
 
 #[derive(Debug, PartialEq)]
-pub enum Value {
+pub enum ElemVal {
     Vacant, // Filler when an element has been "removed" but don't want to shuffle indices
     None,
     Str(String),
@@ -47,7 +46,7 @@ pub struct ElemLookup<'a> {
     lookups: Vec<Lookup<'a>>,
 }
 
-impl <'a> ElemLookup<'a> {
+impl<'a> ElemLookup<'a> {
     pub fn new() -> Self {
         ElemLookup { lookups: vec![] }
     }
@@ -63,18 +62,28 @@ impl <'a> ElemLookup<'a> {
     }
 
     pub fn match_meta(mut self, name: &'a str, value: &'a str) -> Self {
-        self.lookups.push(Lookup::Meta{name, value});
+        self.lookups.push(Lookup::Meta { name, value });
         self
     }
 
-    fn matches(& self, e: & Element) -> bool {
+    fn matches(&self, e: &Element) -> bool {
         for lookup in &self.lookups {
             match *lookup {
-                Lookup::Name(name) => if name != e.name { return false; },
-                Lookup::HasMeta(name) => if !e.meta.contains_key(name) { return false; },
-                Lookup::Meta{name, value} => {
+                Lookup::Name(name) => {
+                    if name != e.name {
+                        return false;
+                    }
+                }
+                Lookup::HasMeta(name) => {
+                    if !e.meta.contains_key(name) {
+                        return false;
+                    }
+                }
+                Lookup::Meta { name, value } => {
                     if let Some(v) = e.meta.get(name) {
-                        if v != value { return false; }
+                        if v != value {
+                            return false;
+                        }
                     }
                 }
             }
@@ -100,7 +109,7 @@ impl ElemHandle {
         self,
         mgr: &mut Manager,
         name: &str,
-        value: Value,
+        value: ElemVal,
     ) -> Result<ElemHandle, String> {
         mgr.create(name, value, Some(self))
     }
@@ -118,14 +127,14 @@ impl Debug for ElemHandle {
 
 struct Element {
     name: String,
-    value: Value,
+    value: ElemVal,
     parent: Option<ElemHandle>,
     children: Vec<ElemHandle>,
     meta: BTreeMap<String, String>,
 }
 
 impl Element {
-    fn new(name: &str, value: Value) -> Self {
+    fn new(name: &str, value: ElemVal) -> Self {
         // New elements start out orphaned and Manager will link appropriately.
         Element {
             name: name.to_owned(),
@@ -140,7 +149,7 @@ impl Element {
 impl Debug for Element {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self.value {
-            Value::Vacant => write!(f, "VACANT"),
+            ElemVal::Vacant => write!(f, "VACANT"),
             _ => {
                 let mut d = f.debug_struct("");
 
@@ -165,28 +174,117 @@ impl Debug for Element {
 
 pub struct JsonLoader {}
 
+struct JsonElem {
+    e: ElemHandle,
+    arr_val: Option<Vec<Value>>,
+    obj_val: Option<Map<String, Value>>,
+}
+
 impl JsonLoader {
+    fn add_elem(
+        frontier: &mut VecDeque<JsonElem>,
+        m: &mut Manager,
+        parent: ElemHandle,
+        val: Value,
+        name: String,
+    ) -> Result<(), String> {
+        if name == "Meta" {
+            match val {
+                Value::Object(o) => {
+                    for (meta_name, meta_val) in o {
+                        if meta_val.is_string() {
+                            m.set_meta(parent, &meta_name, meta_val.as_str().unwrap());
+                        } else {
+                            return Err(format!("Meta value '{meta_name}' under '{}' must have a string value.", m.name(parent)));
+                        }
+                    }
+                }
+                _ => {
+                    return Err(format!("Meta field under '{}' must be an object.", m.name(parent)));
+                }
+            }
+        } else {
+            let mut arr_val: Option<Vec<Value>> = None;
+            let mut obj_val: Option<Map<String, Value>> = None;
+
+            let ev = match val {
+                Value::Null => ElemVal::None,
+                Value::Bool(b) => ElemVal::Bool(b),
+                Value::Number(n) => {
+                    if n.is_f64() {
+                        ElemVal::F64(n.as_f64().unwrap())
+                    } else if n.is_i64() {
+                        ElemVal::I64(n.as_i64().unwrap())
+                    } else {
+                        ElemVal::U64(n.as_u64().unwrap())
+                    }
+                }
+                Value::String(s) => ElemVal::Str(s),
+                Value::Array(a) => {
+                    arr_val = Some(a);
+                    ElemVal::Array
+                }
+                Value::Object(o) => {
+                    obj_val = Some(o);
+                    ElemVal::Object
+                }
+            };
+            
+            let e = parent.new_child(m, &name, ev)?;
+
+            frontier.push_back(JsonElem {
+                e,
+                arr_val,
+                obj_val,
+            });
+        }
+
+        Ok(())
+    }
+
     pub fn from_json(json: &str) -> Result<Manager, String> {
         let v: serde_json::Value = match serde_json::from_str(json) {
             Ok(value) => value,
-            Err(e) => {return Err(format!("{e}"));},
+            Err(e) => {
+                return Err(format!("{e}"));
+            }
+        };
+
+        let obj = if let Value::Object(o) = v { o } else {
+            return Err("JSON value must be an object.".to_owned());
         };
 
         let mut m = Manager::new();
 
-        let root = m.root();
+        let mut frontier: VecDeque<JsonElem> = VecDeque::new();
+        //Self::add_elem(&mut frontier, &mut m, root, v, "".to_owned())?;
+        frontier.push_back(JsonElem {
+            e: m.root(),
+            arr_val: None,
+            obj_val: Some(obj),
+        });
 
-        Self::recurse_json(&mut m, v, root)?;
+        while !frontier.is_empty() {
+            let elem = frontier.pop_front().unwrap();
+            match elem {
+                JsonElem { arr_val: Some(a), .. } => {
+                    let mut i = 0;
+                    for arr_elem in a {
+                        Self::add_elem(&mut frontier, &mut m, elem.e, arr_elem, format!("{i}"))?;
+                        i += 1;
+                    }
+                }
+                JsonElem { obj_val: Some(o), .. } => {
+                    for (obj_name, obj_elem) in o {
+                        Self::add_elem(&mut frontier, &mut m, elem.e, obj_elem, obj_name)?;
+                    }
+                }
+                _ => {}
+            };
+        };
 
         Ok(m)
     }
-
-    // fn recurse_json (m: &mut Manager, v: serde_json::Value, e: ElemHandle) -> Result<(), String> {
-    //     match v {
-    //         serde_json::Value::Null
-    //     }
-    //     todo!()
-    // }
 }
 
 #[derive(Debug)]
@@ -201,7 +299,7 @@ impl Manager {
             elems: vec![
                 // Create root node as first element (idx == 0) to store names of top level elems
                 // It's created as an object since it will hold named elements.
-                Element::new("", Value::Object),
+                Element::new("", ElemVal::Object),
             ],
             vacant: vec![],
         }
@@ -215,7 +313,7 @@ impl Manager {
     pub fn create(
         &mut self,
         name: &str,
-        value: Value,
+        value: ElemVal,
         parent: Option<ElemHandle>,
     ) -> Result<ElemHandle, String> {
         validate_elem_name(name)?;
@@ -290,7 +388,7 @@ impl Manager {
             // The position is tracked in vacant so the now "blank" position can
             //  be used for new elements.
             self.vacant.push(e.0);
-            self.elem_mut(e).value = Value::Vacant;
+            self.elem_mut(e).value = ElemVal::Vacant;
         }
 
         Ok(())
@@ -309,12 +407,12 @@ impl Manager {
 
     /// Returns an immutable reference of an element's value given its handle.
     /// Use `value_mut()` for a mutable reference.
-    pub fn value(&self, e: ElemHandle) -> &Value {
+    pub fn value(&self, e: ElemHandle) -> &ElemVal {
         &self.elem(e).value
     }
 
     /// Returns a mutable reference to an element's value so it can be changed.
-    pub fn value_mut(&mut self, e: ElemHandle) -> &mut Value {
+    pub fn value_mut(&mut self, e: ElemHandle) -> &mut ElemVal {
         &mut self.elem_mut(e).value
     }
 
@@ -335,7 +433,12 @@ impl Manager {
         &mut self.elem_mut(e).children
     }
 
-    pub fn descendents(&self, e: ElemHandle, lookup: &ElemLookup, look_past_match: bool) -> Vec<ElemHandle> {
+    pub fn descendents(
+        &self,
+        e: ElemHandle,
+        lookup: &ElemLookup,
+        look_past_match: bool,
+    ) -> Vec<ElemHandle> {
         let mut q = VecDeque::new();
         let mut found = Vec::new();
 
@@ -348,7 +451,9 @@ impl Manager {
             let e = q.pop_front().unwrap();
             let matches = lookup.matches(self.elem(e));
 
-            if matches { found.push(e) };
+            if matches {
+                found.push(e)
+            };
 
             if !matches || look_past_match {
                 for c in self.children(e) {
@@ -364,7 +469,13 @@ impl Manager {
         let mut e = self.elem(e);
         loop {
             match e.parent {
-                Some(ph) => if lookup.matches(self.elem(ph)) { return Some(ph) } else { e = self.elem(ph) },
+                Some(ph) => {
+                    if lookup.matches(self.elem(ph)) {
+                        return Some(ph);
+                    } else {
+                        e = self.elem(ph)
+                    }
+                }
                 None => return None,
             }
         }
@@ -391,7 +502,7 @@ impl Manager {
             if !child_found {
                 let result = self.create(
                     *name,
-                    Value::None,
+                    ElemVal::None,
                     if !e.is_root() { Some(e) } else { None },
                 );
                 e = match result {
@@ -447,7 +558,7 @@ impl Manager {
     /// Returns whether or not the element handle is a vacant slot.
     /// This means the handle is not valid for other operations.
     pub fn is_vacant(&self, e: ElemHandle) -> bool {
-        self.elem(e).value == Value::Vacant
+        self.elem(e).value == ElemVal::Vacant
     }
 
     /// Returns the name hierarchy of the specified element index.
@@ -478,27 +589,27 @@ mod tests {
     #[test]
     fn empty_element_name_fails() {
         let mut m = Manager::new();
-        assert!(m.create("", Value::None, None).is_err());
+        assert!(m.create("", ElemVal::None, None).is_err());
     }
 
     #[test]
     fn reserved_element_name_fails() {
         let mut m = Manager::new();
-        assert!(m.create("Meta", Value::None, None).is_err());
-        assert!(m.create("Value", Value::None, None).is_err());
+        assert!(m.create("Meta", ElemVal::None, None).is_err());
+        assert!(m.create("Value", ElemVal::None, None).is_err());
     }
 
     #[test]
     fn reserved_rename_fails() {
         let mut m = Manager::new();
-        let e = m.create("alpha", Value::None, None).unwrap();
+        let e = m.create("alpha", ElemVal::None, None).unwrap();
         assert!(m.rename(e, "Meta").is_err());
     }
 
     #[test]
     fn rename_element() {
         let mut m = Manager::new();
-        let e = m.create("alpha", Value::None, None).unwrap();
+        let e = m.create("alpha", ElemVal::None, None).unwrap();
         m.rename(e, "beta").unwrap();
         assert_eq!(m.name(e), "beta");
     }
@@ -507,7 +618,7 @@ mod tests {
     fn create_element() {
         let mut m = Manager::new();
         let e = m
-            .create("alpha", Value::Str(String::from("Meowdy")), None)
+            .create("alpha", ElemVal::Str(String::from("Meowdy")), None)
             .unwrap();
         assert_eq!(m.name(e), "alpha");
     }
@@ -515,8 +626,10 @@ mod tests {
     #[test]
     fn create_child_get_hierarchy() {
         let mut m = Manager::new();
-        let parent = m.create("parent", Value::I32(42), None).unwrap();
-        let child = m.create("child", Value::Bool(true), Some(parent)).unwrap();
+        let parent = m.create("parent", ElemVal::I32(42), None).unwrap();
+        let child = m
+            .create("child", ElemVal::Bool(true), Some(parent))
+            .unwrap();
 
         assert_eq!(m.hierarchy(child), vec!["parent", "child"]);
     }
@@ -524,8 +637,8 @@ mod tests {
     #[test]
     fn traverse_to_parent() {
         let mut m = Manager::new();
-        let p = m.create("parent", Value::None, None).unwrap();
-        let c = m.create("child", Value::None, Some(p)).unwrap();
+        let p = m.create("parent", ElemVal::None, None).unwrap();
+        let c = m.create("child", ElemVal::None, Some(p)).unwrap();
 
         let tp = m.parent(c).unwrap();
         assert_eq!(m.name(tp), "parent");
@@ -534,25 +647,25 @@ mod tests {
     #[test]
     fn lookup_hierarchy() {
         let mut m = Manager::new();
-        let p = m.create("parent", Value::None, None).unwrap();
-        let _c = m.create("child", Value::I32(42), Some(p)).unwrap();
+        let p = m.create("parent", ElemVal::None, None).unwrap();
+        let _c = m.create("child", ElemVal::I32(42), Some(p)).unwrap();
 
         let e = m.lookup(&vec!["parent", "child"]).unwrap();
-        assert_eq!(*m.value(e), Value::I32(42));
+        assert_eq!(*m.value(e), ElemVal::I32(42));
     }
 
     #[test]
     fn change_value() {
         let mut m = Manager::new();
-        let e = m.create("alpha", Value::None, None).unwrap();
-        *m.value_mut(e) = Value::Bool(true);
-        assert_eq!(*m.value(e), Value::Bool(true));
+        let e = m.create("alpha", ElemVal::None, None).unwrap();
+        *m.value_mut(e) = ElemVal::Bool(true);
+        assert_eq!(*m.value(e), ElemVal::Bool(true));
     }
 
     #[test]
     fn create_via_obtain() {
         let mut m = Manager::new();
-        let _ = m.create("alpha", Value::None, None).unwrap();
+        let _ = m.create("alpha", ElemVal::None, None).unwrap();
         let e = m.obtain(&vec!["alpha", "beta", "gamma"]).unwrap();
         assert_eq!(m.hierarchy(e), vec!["alpha", "beta", "gamma"]);
         assert_eq!(m.len(), 3);
@@ -561,10 +674,10 @@ mod tests {
     #[test]
     fn remove_subtree() {
         let mut m = Manager::new();
-        let a = m.create("a", Value::None, None).unwrap();
-        let b = m.create("b", Value::None, Some(a)).unwrap();
-        let c = m.create("c", Value::None, Some(a)).unwrap();
-        let d = m.create("d", Value::None, Some(c)).unwrap();
+        let a = m.create("a", ElemVal::None, None).unwrap();
+        let b = m.create("b", ElemVal::None, Some(a)).unwrap();
+        let c = m.create("c", ElemVal::None, Some(a)).unwrap();
+        let d = m.create("d", ElemVal::None, Some(c)).unwrap();
 
         m.remove(c).unwrap();
 
@@ -576,7 +689,7 @@ mod tests {
     #[test]
     fn set_get_meta() {
         let mut m = Manager::new();
-        let e = m.create("a", Value::None, None).unwrap();
+        let e = m.create("a", ElemVal::None, None).unwrap();
         m.set_meta(e, "VL_Type", "VL_Task");
 
         assert!(m.has_meta(e, "VL_Type"));
@@ -586,7 +699,7 @@ mod tests {
     #[test]
     fn get_missing_meta() {
         let mut m = Manager::new();
-        let e = m.create("a", Value::None, None).unwrap();
+        let e = m.create("a", ElemVal::None, None).unwrap();
 
         assert!(!m.has_meta(e, "VL_Type"));
         assert!(m.get_meta(e, "VL_Type").is_none());
@@ -595,9 +708,8 @@ mod tests {
     #[test]
     fn match_meta_name() {
         let mut m = Manager::new();
-        let e = m.create("a", Value::None, None).unwrap();
-        let l = ElemLookup::new()
-            .match_name("a");
+        let e = m.create("a", ElemVal::None, None).unwrap();
+        let l = ElemLookup::new().match_name("a");
 
         assert!(l.matches(m.elem(e)));
     }
@@ -605,10 +717,10 @@ mod tests {
     #[test]
     fn descendent_meta() {
         let mut m = Manager::new();
-        let a = m.create("a", Value::None, None).unwrap();
-        let b = a.new_child(&mut m, "b", Value::None).unwrap();
-        let c = b.new_child(&mut m, "c", Value::None).unwrap();
-        let d = c.new_child(&mut m, "d", Value::None).unwrap();
+        let a = m.create("a", ElemVal::None, None).unwrap();
+        let b = a.new_child(&mut m, "b", ElemVal::None).unwrap();
+        let c = b.new_child(&mut m, "c", ElemVal::None).unwrap();
+        let d = c.new_child(&mut m, "d", ElemVal::None).unwrap();
 
         m.set_meta(a, "VL_Type", "VL_Task");
         m.set_meta(b, "VL_Type", "VL_Channel");
@@ -620,9 +732,62 @@ mod tests {
         let chans = m.descendents(a, &l, true);
 
         assert_eq!(chans.len(), 2);
-        
+
         let names: Vec<&str> = chans.iter().map(|e| m.name(*e)).collect();
         assert!(names.contains(&m.name(b)));
         assert!(names.contains(&m.name(d)));
+    }
+
+    #[test]
+    fn basic_parse_json() {
+        let json = r#"
+{
+    "Volatus": {
+        "Meta": {"VL_Config_Version": "0.0.0-dev"},
+        "TestSystem": {
+            "Meta": {"VL_Type": "VL_System"},
+            "Clusters": {
+                "TestCluster": {
+                    "Meta": {"VL_Type": "VL_Cluster"},
+                    "Telemetry": {"Routing": "Unicast", "Endpoint": {"Address": "127.0.0.1", "Port": 36985} },
+                    "Groups": {"Events": 5, "Config": 6, "Logging": 7, "Clients": 8},
+                    "Nodes": {
+                        "Test_DAQ": {
+                            "Meta": {"VL_Type": "VL_Node"},
+                            "Node_ID": 1,
+                            "Network": {
+                                "Bind_Address": "0.0.0.0",
+                                "TCP": {"Address": "127.0.0.1", "Port": 12081, "Server": true},
+                                "HTTP_Port": 13081
+                            },
+                            "Tasks": {
+                                "Telem_Server": {"Meta": {"VL_Type": "VL_Task", "VL_Task_Type": "TelemetryServer"}}
+                            }
+                        },
+                        "Test_GUI": {
+                            "Meta": {"VL_Type": "VL_Node"},
+                            "Node_ID": 2,
+                            "Network": {
+                                "Bind_Address": "0.0.0.0",
+                                "TCP": {"Address": "127.0.0.1", "Port": 12081, "Server": false},
+                                "HTTP_Port": 13081
+                            },
+                            "Tasks": {
+                                "GUI_Manager": {
+                                    "Meta": {"VL_Type": "VL_Task", "VL_Task_Type": "GUIManager"},
+                                    "Plugins": "gui",
+                                    "Title": "Volatus Test"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}"#;
+
+        let m = JsonLoader::from_json(json).unwrap();
+        println!("{m:#?}");
     }
 }
--- crates/volatus/src/main.rs
+++ crates/volatus/src/main.rs
@@ -1,17 +1,17 @@
-use volatus::config::{Manager, Value};
+use volatus::config::{Manager, ElemVal};
 
 fn main() {
     let mut m = Manager::new();
 
-    let p = m.create("alpha", Value::I32(42), None).unwrap();
+    let p = m.create("alpha", ElemVal::I32(42), None).unwrap();
 
-    let c1 = p.new_child(&mut m, "beta", Value::Bool(true)).unwrap();
+    let c1 = p.new_child(&mut m, "beta", ElemVal::Bool(true)).unwrap();
     
     let l = c1
-        .new_child(&mut m, "gamma", Value::Str("Meowdy".to_owned()))
+        .new_child(&mut m, "gamma", ElemVal::Str("Meowdy".to_owned()))
         .unwrap();
-    
-    let _c2 = p.new_child(&mut m, "delta", Value::Bool(false)).unwrap();
+
+    let _c2 = p.new_child(&mut m, "delta", ElemVal::Bool(false)).unwrap();
 
     m.remove(l).unwrap();
 
